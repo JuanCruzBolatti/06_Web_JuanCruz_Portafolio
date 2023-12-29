740c73204a504f5d822a650a3c132439e76a80ef
JuanCruzBolatti <juancruzbolatti.t@gmail.com>
2023-12-28 23:02:43 -0300

    new menu display

--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,15 @@ function getScrollYOffset(element) {
     return rect.top + scrollTop;
 }
 
+function scrollTo(element) {
+    document.body.scroll({
+        top: getScrollYOffset(element)
+    });
+    document.documentElement.scroll({
+        top: getScrollYOffset(element)
+    });
+}
+
 // Logo
 var logos = document.querySelector(".logo-container").getElementsByTagName('img');
 
@@ -27,5 +36,10 @@ window.onscroll = () => {
 // Toggle Navbar
 function toggleNav(navbar) {
     navbar.classList.toggle('displayed');
-    
+
+}
+
+// Navbar Anchors
+function anchorTo(element) {
+    scrollTo(document.querySelector(element.getAttribute("href")));
 }
